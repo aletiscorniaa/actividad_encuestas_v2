d578ed9f142a50af5cfd3f8d907bf20521592a53
Ale <tiscorniaale@gmail.com>
2023-10-20 02:48:35 -0300

    first commit

---
+++ src/Components/CrearEncuesta.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
+const CrearEncuesta = ({ agregarEncuesta }) => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const navigate = useNavigate();
+    const onSubmit = (data) => {
+        agregarEncuesta(data)
+        navigate('/');
+    };
+    return (
+        <div>
+            <h1>Crear Nueva Encuesta</h1>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <label>Título:</label>
+                <input
+                    type="text"
+                    id="titulo"
+                    name="titulo"
+                    {...register("titulo", {
+                        required: 'Este campo es obligatorio',
+                        maxLength: {
+                            value: 50, message: 'El título debe tener menos de 50 caracteres'
+                        },
+                        minLength: {
+                            value: 5, message: 'La descripcion debe tener al menos 5 caracteres'
+                        },
+                        pattern: {
+                            value: /^(?!\s*$).+/,
+                            message: 'La descripción no puede consistir solo en espacios en blanco'
+                        }
+                    })}
+                />
+                {errors.titulo && <p>{errors.titulo.message}</p>}
+                <label>Descripción:</label>
+                <textarea
+                    id="descripcion"
+                    name="descripcion"
+                    {...register("descripcion", {
+                        maxLength: {
+                            value: 200, message: 'La descripción debe tener menos de 200 caracteres'
+                        },
+                        minLength: {
+                            value: 5, message: 'La descripcion debe tener al menos 5 caracteres'
+                        },
+                        pattern: {
+                            value: /^(?!\s*$).+/,
+                            message: 'La descripción no puede consistir solo en espacios en blanco'
+                        }
+                    })}
+                />
+                {errors.descripcion &&
+                    <p>{errors.descripcion.message}</p>}
+                {/* Agrega aquí campos adicionales y sus validaciones según sea necesario */}
+                <button type="submit">Guardar Encuesta</button>
+            </form>
+        </div>
+    );
+};
+export default CrearEncuesta;
